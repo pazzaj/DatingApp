81d0b47ed58f26e8478d8842db756495f5716d9b
Pat Jamroz <patrykjamroz01@gmail.com>
2023-06-22 20:13:14 +0100

    Updating Resources

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -4,6 +4,7 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -13,6 +14,9 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
   imports: [
     CommonModule,
     NgxGalleryModule,
+    NgxSpinnerModule.forRoot({
+      type:'ball-newton-cradle'
+    }),
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),     
     ToastrModule.forRoot({
@@ -24,7 +28,8 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
     BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxGalleryModule   
+    NgxGalleryModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
